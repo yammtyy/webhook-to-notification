c515f4403556984aa57980d08783269127cdb1dc
雄哉 山本 <yamamoto_y@orc-net.com>
2021-09-30 18:11:08 +0900

    projectの追加

---
+++ AppBackend/AppBackend/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace AppBackend
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API の設定およびサービス
+
+            // Web API ルート
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // メッセージハンドラー
+            config.MessageHandlers.Add(new AuthenticationTestHandler());
+        }
+    }
+}
